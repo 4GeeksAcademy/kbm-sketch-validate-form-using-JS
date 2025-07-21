d4eea8ed3deb93b16368065d5d9d622f6d53f848
kbm-sketch <kbretmanna@gmail.com>
2025-07-21 22:07:24 +0000

    changed title

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
    const form = document.getElementById("myForm");
